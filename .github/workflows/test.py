407e6bf305f083afea17857e5c43864932f1bdd6
Ekaterina20092004 <74540968+Ekaterina20092004@users.noreply.github.com>
2024-05-08 23:26:11 +0300

    Create test.py

---
+++ .github/workflows/test.py
@@ -0,0 +1,17 @@
+import unittest
+from ../../main.py import add
+
+class TestAddFunction(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        self.assertEqual(add(1, 2), 3)
+
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-1, -2), -3)
+
+    def test_add_mixed_numbers(self):
+        self.assertEqual(add(1, -2), -1)
+        self.assertEqual(add(-1, 2), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
