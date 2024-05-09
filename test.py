6c6d43ad628f837033a88bc93463921baba6f535
Ekaterina20092004 <Leonteva.Ekaterina20@yandex.ru>
2024-05-09 17:28:34 +0300

    move

---
+++ test.py
@@ -0,0 +1,18 @@
+import unittest
+from main import add
+
+
+class TestAddFunction(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        self.assertEqual(add(1, 2), 3)
+
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-1, -2), -3)
+
+    def test_add_mixed_numbers(self):
+        self.assertEqual(add(1, -2), -1)
+        self.assertEqual(add(-1, 2), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
