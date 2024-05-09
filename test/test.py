6610a0becc230309aefa237818acb0e5714e980b
Ekaterina20092004 <Leonteva.Ekaterina20@yandex.ru>
2024-05-09 18:11:56 +0300

    update

---
+++ test/test.py
@@ -0,0 +1,15 @@
+import unittest
+import main
+
+
+class TestAddFunction(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        self.assertEqual(main.add(1, 2), 3)
+
+    def test_add_negative_numbers(self):
+        self.assertEqual(main.add(-1, -2), -3)
+
+    def test_add_mixed_numbers(self):
+        self.assertEqual(main.add(1, -2), -1)
+        self.assertEqual(main.add(-1, 2), 1)
+
